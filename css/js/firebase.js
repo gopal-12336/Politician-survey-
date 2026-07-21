d6d71e5fb5227b6c941d457dc90f81625fdf338e
gopal-12336 <goswamivinod2305@gmail.com>
2026-07-21 10:46:19 +0530

    Update firebase.js

--- css/js/firebase.js
+++ css/js/firebase.js
@@ -1,1 +1,26 @@
+// Firebase Configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCVhwl6lA5Q-fiKC8dzUIxHXXG4DiQywGk",
+  authDomain: "politician-survey.firebaseapp.com",
+  projectId: "politician-survey",
+  storageBucket: "politician-survey.firebasestorage.app",
+  messagingSenderId: "802387421098",
+  appId: "1:802387421098:web:2c3e5b8a8692a3a2d22dd3"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Authentication
+const auth = firebase.auth();
+const db = firebase.firestore();
+// reCAPTCHA
+window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
+    'recaptcha-container',
+    {
+        size: 'normal'
+    }
+);
+
+recaptchaVerifier.render();
+document.getElementById("testMessage").innerHTML = "Firebase Loaded Successfully";
